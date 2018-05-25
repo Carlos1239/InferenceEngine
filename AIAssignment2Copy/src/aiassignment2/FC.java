1dadf8b586815805ebbcc4fb6014da7b161745c1
Carlos1239 <CLwilson1431@hotmail.com>
2018-05-25 13:41:06 +1000

    Update FC.java

--- AIAssignment2Copy/src/aiassignment2/FC.java
+++ AIAssignment2Copy/src/aiassignment2/FC.java
@@ -7,6 +7,8 @@ package aiassignment2;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.ListIterator;
+
 
 /**
  *
@@ -16,7 +18,9 @@ public class FC extends SearchMethod {
     ArrayList<String> agenda;
     ArrayList<String> clauses;
     ArrayList<Integer> count;
+    ArrayList<String> sym;
     public FC(){
+    
     code= "FC";
     entails = new ArrayList<>();
     agenda= new ArrayList<>();
@@ -28,11 +32,10 @@ public class FC extends SearchMethod {
     @Override
     public String methodOutput(boolean entail) {
        String result;
+      
        if(entail==true){
            result = "YES:";
-           for(int i=0; i<entails.size();i++){
-               result += entails.get(i)+",";
-           }
+           result = entails.stream().map((i) -> i).map((ent) -> ent+",").reduce(result, String::concat);
            result += a;
        }
        else{
@@ -50,20 +53,28 @@ public class FC extends SearchMethod {
         count = kb.getCount();
         a=query;
         while(!agenda.isEmpty()){
-            String ask = agenda.remove(0);
-            entails.add(ask);
-            for(int i=0;i<clauses.size();i++){
-                if(premiseContains(clauses.get(i),ask)){
-                    Integer x = count.get(i);
-                    count.set(i,--x);
-                    if(count.get(i)==0){
-                        String prove = clauses.get(i).split("=>")[1];
-                        if(prove.equals(query))
-                            return true;
-                        agenda.add(prove);
+            ListIterator itc = clauses.listIterator();
+            
+            while(itc.hasNext()){
+          
+                String ask = agenda.remove(0);
+                entails.add(ask);
+                for(int i=0;i<clauses.size();i++){
+                    if(premiseContains(clauses.get(i),ask)){
+                        Integer x = count.get(i);
+                        count.set(i,--x);
+                        if(count.get(i)==0){
+                            String prove = clauses.get(i).split("=>")[1];
+                            if(prove.equals(query)){
+                                return true;
+                            }
+                            agenda.add(prove);
+                        }
                     }
                 }
-            }
+            itc.next();
+        }
+            
         }
         return false;
     }
@@ -79,10 +90,5 @@ public class FC extends SearchMethod {
             return Arrays.asList(conjuncts).contains(ask);     
         }
     }
-    
-    
-    
-    
-    
-    
+   
 }
