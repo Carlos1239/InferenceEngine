5eca32779483537ee292354b706e4de71eb6c052
Carlos1239 <CLwilson1431@hotmail.com>
2018-05-25 13:02:29 +1000

    Update TT.java

--- AIAssignment2Copy/src/aiassignment2/TT.java
+++ AIAssignment2Copy/src/aiassignment2/TT.java
@@ -12,50 +12,53 @@ import java.util.ArrayList;
  * @author Ann
  */
 public class TT extends SearchMethod{
+    int ValidModels;
     public TT(){
         code="TT";
     }
 
     @Override
     public String methodOutput(boolean sa) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String result = Integer.toString(ValidModels);
+        if(sa==true){
+            
+            return "YES:" + result;
+        }
+        else {
+            return "No";
+        }
     }
 
     @Override
     public boolean methodEntails(String query, KBase kb) {
-        int ValidModels = 0;
-		boolean[] models = new boolean[kb.count.length] //number of symbols
+        ValidModels = 0;
+		boolean[] models = new boolean[kb.getSymbols().size()]; //number of symbols
 		int temp = models.length;
 		int ModSize = (temp)^2; //total number of permutations
 		for (int i =0; i< ModSize; i++){ // for each permutation of symbols
 			// set true/false
-			for (int j =0; j< kb.symbols.length; j++){ //for each symbol in the permutation
-				models[j] = ((i&(1<<j)) !=0); //set the true and false values by shifting 1/0 to the correct positons
+			for (int j =0; j< kb.getSymbols().size(); j++){ //for each symbol in the permutation
+				models[j] = ((i&(1<<j)) !=0);
 			}
 			boolean allTrue = true;
 			// check kb is true
-			for (int j =0; j< kb.agenda.length; j++){ //for each sentence
-				if(!models[j]) { //
+			for (int j =0; j< kb.getClauses().size(); j++){//for each sentence
+				if(!models[j]) {
 					allTrue = false;
 				}
 			}
 			//check if ask is true
 			boolean ask = false;
-			if (askIsTrue()) {
-				ask = true;
-			}
+//			if (askIsTrue()) {
+//				ask = true;
+//			}
 			
 			if (allTrue && ask){
 				ValidModels ++;
 			}
 		}
 		
-		if (ValidModels > 0){
-			System.out.println('YES:' + ValidModels);
-		}
-		else{
-			System.out.println('NO');
-		}
+        return ValidModels > 0;
     }
 
     @Override
